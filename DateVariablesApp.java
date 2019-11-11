177c123fc7a805e76aa71d874fb6e3c331f33037
petrpublic <57359625+petrpublic@users.noreply.github.com>
2019-11-11 15:54:57 +0300

    Add files via upload

---
+++ DateVariablesApp.java
@@ -0,0 +1,32 @@
+
+public class DateVariablesApp {
+
+	public static void main(String[] args) {
+		
+		int todayisYear = 2019;
+		int todayisMonth = 11; 
+		int todayisDay = 11;
+		
+		System.out.print("Today is: ");
+		System.out.print(todayisYear);
+		System.out.print("-");
+		System.out.print(todayisMonth);
+		System.out.print("-");
+		System.out.print(todayisDay);
+		
+     /**print-это печать в строку 
+	  println-это курсор будет перевудён на новую строку*/
+		
+		
+		char    c = '#';
+		boolean b = true; /*false;*/
+		byte   by = -127; /*128;*/
+		short   s = -30000; /*30000;*/
+		int     i = -2000000000; /*2000000000;*/    
+		long    l = -1000000000000000000l;/*1000000000000000000l;*/
+		float   f = -999999999999999999999999999999999f;/**/
+		double  d = 9e50;
+
+	}
+
+}
